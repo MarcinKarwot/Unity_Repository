4102bf2817f612b4f285eb6788de47554319b504
Marcin Karwot <Mar.Karwot@gmail.com>
2023-04-09 23:07:14 +0200

    ChangedScript - bugfix 1001 - refactorisation

--- Unity_TextGame/Assets/Scripts/TextGame.cs
+++ Unity_TextGame/Assets/Scripts/TextGame.cs
@@ -16,13 +16,14 @@ public class TextGame : MonoBehaviour
     void Start()
     {
         //typ nazwe i ew. podac poczatkowa wartosc
-        
 
-        Debug.Log("Witaj w grze - pomysl w glowie liczbe od 1 do 1000");
+
+        Debug.Log("Witaj w grze - pomysl w glowie liczbe od "+min+ "do" + max );
+        max = 1001;
         Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od 500?");
         //print string
         //Nacisnij gorna strzalke aby odpowiedziec ze wieksza
-        
+
     }
 
     // Update is called once per frame
@@ -31,18 +32,26 @@ public class TextGame : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = shoot;
-            shoot = (min + max) / 2;
-            Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od " + shoot + "?");
+            NextShoot();
+
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = shoot;
-            shoot = (min + max) / 2;
-            Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od " + shoot + "?");
+            NextShoot();
+
+
         }
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Komputer zgadl liczbe");
         }
     }
+
+    void NextShoot()
+    {
+        shoot = (min + max) / 2;
+        Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od " + shoot + "?");
+    }
+
 }
